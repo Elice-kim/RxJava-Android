46fa08ceaad227626e99c79a5173aee5eb9d5ad7
Elice-kim <grit_julia@naver.com>
2017-07-12 22:44:42 +0900

    view binding with Butterknife

--- RxAndroid/app/src/main/java/com/rxandroid/elice/rxandroid/activity/LoginActivity.java
+++ RxAndroid/app/src/main/java/com/rxandroid/elice/rxandroid/activity/LoginActivity.java
@@ -1,15 +1,44 @@
 package com.rxandroid.elice.rxandroid.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 import com.rxandroid.elice.rxandroid.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import rx.Subscription;
+
 public class LoginActivity extends AppCompatActivity {
 
+    @BindView(R.id.idEditText)
+    EditText idEditText;
+    @BindView(R.id.passwordEditText)
+    EditText passwordEditText;
+
+    private Subscription subscription;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
     }
+
+    @OnClick(R.id.loginButton)
+    void loginButtonClicked() {
+        if (isNotValid()) {
+            return;
+        }
+        requestLogin();
+    }
+
+    private boolean isNotValid() {
+        return true;
+    }
+
+    private void requestLogin() {
+    }
+
 }
