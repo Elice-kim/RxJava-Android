2158494e8c3bea694ac69f6e9102272e10a1eb4d
김나형 <nahyung92@gmail.com>
2017-07-12 22:27:40 +0900

    add RxAndroid and Retrofit Library & packaging

---
+++ RxAndroid/app/src/main/java/com/rxandroid/elice/rxandroid/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.rxandroid.elice.rxandroid.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.rxandroid.elice.rxandroid.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
